4d06d9b7865347e425ac787c72f5d301e5a34d78
alex8937 <ultraseven8937@gmail.com>
2018-04-30 11:07:44 -0400

    Added new feature: fetch probelm title

--- Schedule.py
+++ Schedule.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import numpy as np
 from datetime import date
 from math import ceil
+import json, requests
 
 class Schedule:
 
@@ -92,7 +93,7 @@ class Schedule:
  def __set_date():
   try:
    day = input("Input date (Enter for today | 'q' for Quit): e.g. 2018-04-03 or 20180403: \n")
-   if day == "q":     
+   if day == "q":
     return -1
    if day == "":
     day = pd.to_datetime(date.today())
@@ -106,14 +107,14 @@ class Schedule:
 
  def __get_problems(self):
   try:
-   day = input("Input date to show problems (Enter for today | 'q' for Quit): e.g. 2018-04-03 or 20180403: \n")   
+   day = input("Input date to show problems (Enter for today | 'q' for Quit): e.g. 2018-04-03 or 20180403: \n")
    if day == 'q':
     return -1
    if day == '':
     day = date.today()
    day = str(pd.to_datetime(day, format='%Y-%m-%d').date())
    print('TO DO: {} ({})'.format(' '.join(map(str, self.data.loc[day, 'Todo'])), len(self.data.loc[day, 'Todo'])))
-   print('NEW: {} ({})'.format(' '.join(map(str, self.data.loc[day, 'Problems'])), len(self.data.loc[day, 'Problems']))) 
+   print('NEW: {} ({})'.format(' '.join(map(str, self.data.loc[day, 'Problems'])), len(self.data.loc[day, 'Problems'])))
    print('')
   except (KeyError, ValueError) as error:
    print("Sorry, I didn't understand that.")
@@ -127,22 +128,47 @@ class Schedule:
   for num in nums:
    ans += Schedule.__flatten(num)
   return ans
- 
-     
+
+ def __get_title():
+  try:
+   problem_id = input("Input problem id to show title ('s' for Skip | 'q' for Quit): e.g. 12: \n")
+   if problem_id == 'q':
+    return -1
+   elif problem_id == 's':
+    return -2
+   else:
+    problem_id = int(problem_id)
+    req = requests.get(url='https://leetcode.com/api/problems/algorithms/')
+    data_json = json.loads(req.text)
+    alg_list = data_json['stat_status_pairs']
+    for alg_json in alg_list:
+     if alg_json['stat']['question_id'] == problem_id:
+      print('\"' + alg_json['stat']['question__title'] + '\"\n')
+      return Schedule.__get_title()
+  except ValueError:
+   print("Sorry, I didn't understand that.")
+   return Schedule.__get_title()
+  except KeyError:
+   print("Oops... problem id not found.")
+   return Schedule.__get_title()
+
  def work_flow(self):
-  quit_flag = False     
-  self.data = Schedule.__pre_process(self.data)   
+  quit_flag = False
+  self.data = Schedule.__pre_process(self.data)
   get_flag = self.__get_problems() if Path(self.path).is_file() else 0
-  quit_flag = (get_flag == -1)   
+  quit_flag = (get_flag == -1)
   while not quit_flag:
+   quit_flag = Schedule.__get_title()
+   if quit_flag == -1:
+    break
    problems = Schedule.__set_problems()
    if problems == -1:
     quit_flag = True
     break
    day = Schedule.__set_date()
    if day == -1:
-    quit_flag = True       
-    break   
+    quit_flag = True
+    break
    input_key = input("Enter 'q' or 'Enter' to complete. If you made some mistakes, press any other key to re-enter \n")
    if input_key == "q" or input_key == "":
     break
